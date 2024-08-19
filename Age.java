da4dacb2ee817cede8b95ea2696386a6a6a41555
Pranav Khot <161944546+Pranav0913@users.noreply.github.com>
2024-08-19 19:51:29 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,12 @@
+import java.util.Scanner.*;
+import java.time.Localdate.*;
+import java.time.Period.*;
+
+public class Age{
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in)
+
+        System.out.println()
+    }
+}
